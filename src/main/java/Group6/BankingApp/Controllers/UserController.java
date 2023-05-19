33d750c3e1b27902039a3fd9ebbd599f177ec54c
AbdelZN <54932229+AbdelZN@users.noreply.github.com>
2023-05-19 19:08:54 +0200

    added dependancies

--- src/main/java/Group6/BankingApp/Controllers/UserController.java
+++ src/main/java/Group6/BankingApp/Controllers/UserController.java
@@ -1,9 +1,49 @@
 package Group6.BankingApp.Controllers;
 
-import org.springframework.stereotype.Controller;
+import Group6.BankingApp.Models.User;
+import Group6.BankingApp.Services.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/users")
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
+
+    private  final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        try{
+            return ResponseEntity.ok().body(userService.getAllUsers());
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(Long id) {
+        try{
+            return ResponseEntity.ok().body(userService.getUserById(id));
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+
+
+
 }
