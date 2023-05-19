a83d1acb87343455a77948a3d17f4fd5d5744203
AbdelZN <54932229+AbdelZN@users.noreply.github.com>
2023-05-20 01:29:14 +0200

    CRUD for Users Complete

--- src/main/java/Group6/BankingApp/Controllers/UserController.java
+++ src/main/java/Group6/BankingApp/Controllers/UserController.java
@@ -1,14 +1,12 @@
 package Group6.BankingApp.Controllers;
 
 import Group6.BankingApp.Models.User;
+import Group6.BankingApp.Models.dto.UserDTO;
 import Group6.BankingApp.Services.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,7 +31,7 @@ public class UserController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getUserById(Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
         try{
             return ResponseEntity.ok().body(userService.getUserById(id));
         }catch (Exception e){
@@ -42,8 +40,32 @@ public class UserController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity addUser(@RequestBody UserDTO userDTO) {
+        try{
+            User newUser=userService.addUser(userDTO);
+            return ResponseEntity.status(201).body(newUser);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getCause().getMessage());
+        }
+    }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        try{
+            User user=userService.updateUser(id, userDTO);
+            return ResponseEntity.ok().body(user);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        try{
+            userService.deleteUser(id);
+            return ResponseEntity.ok().build();
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/Group6/BankingApp/Services/UserService.java
+++ src/main/java/Group6/BankingApp/Services/UserService.java
@@ -1,7 +1,10 @@
 package Group6.BankingApp.Services;
 
 import Group6.BankingApp.DAL.UserRepository;
+import Group6.BankingApp.Models.Role;
 import Group6.BankingApp.Models.User;
+import Group6.BankingApp.Models.dto.UserDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,15 +24,26 @@ public class UserService {
     }
 
     public User getUserById(Long id) {
+
         return userRepository.findById(id).orElse(null);
     }
 
-    public void addUser(User user) {
-        userRepository.save(user);
+    public User addUser(UserDTO userDTO) {
+        return userRepository.save(new User(userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(), userDTO.getPassword(), userDTO.getPhoneNumber(), Role.CUSTOMER));
     }
 
-    public void updateUser(User user) {
-        userRepository.save(user);
+    public User updateUser(Long id,UserDTO user) {
+        try{
+            User userToUpdate=userRepository.findById(id).orElse(null);
+            userToUpdate.setFirstName(user.getFirstName());
+            userToUpdate.setLastName(user.getLastName());
+            userToUpdate.setEmail(user.getEmail());
+            userToUpdate.setPassword(user.getPassword());
+            userToUpdate.setPhoneNumber(user.getPhoneNumber());
+            return  userRepository.save(userToUpdate);
+        }catch (Exception e){
+            throw new EntityNotFoundException("User not found");
+        }
     }
 
     public void deleteUser(Long id) {
