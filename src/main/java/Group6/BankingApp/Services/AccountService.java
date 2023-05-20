5a81b4caa3cd1e34b12598fd7c2f17a1428d6f21
MhdMokhtar <m.aboudaher91@gmail.com>
2023-05-20 11:40:53 +0200

    Saturday

---
+++ src/main/java/Group6/BankingApp/Services/AccountService.java
@@ -0,0 +1,38 @@
+package Group6.BankingApp.Services;
+
+import Group6.BankingApp.DAL.AccountRepository;
+import Group6.BankingApp.DAL.UserRepository;
+import Group6.BankingApp.Models.Account;
+import Group6.BankingApp.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public AccountService() {
+    }
+
+    public List<Account> getAllAccounts() {
+        return (List<Account>) accountRepository.findAll();
+    }
+
+    public Account getAccountById(String iban) {
+        return accountRepository.findById(iban).orElse(null);
+    }
+
+    public void addAccount(Account account) {
+        accountRepository.save(account);
+    }
+
+    public void updateAccount(Account account) {
+        accountRepository.save(account);
+    }
+
+    public void deleteAccount(String iban) {
+        accountRepository.deleteById(iban);
+    }
+}
