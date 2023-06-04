18eeda1ed2708766a4cce49e02d9f8ec2c93832a
OhannesH <90188159+OhannesH@users.noreply.github.com>
2023-06-04 19:52:50 +0200

    added sufficient funds check

--- src/main/java/Group6/BankingApp/Services/TransactionService.java
+++ src/main/java/Group6/BankingApp/Services/TransactionService.java
@@ -2,6 +2,9 @@ package Group6.BankingApp.Services;
 
 import Group6.BankingApp.DAL.TransactionRepository;
 import Group6.BankingApp.Models.Transaction;
+import Group6.BankingApp.Services.AccountService;
+import Group6.BankingApp.Models.User;
+import Group6.BankingApp.Models.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import jakarta.persistence.EntityNotFoundException;
@@ -10,9 +13,11 @@ import java.util.List;
 
 @Service
 public class TransactionService {
-
+    @Autowired
+    AccountService accountService;
     @Autowired
     private TransactionRepository transactionRepository;
+    
 
     public TransactionService() {
     }
@@ -27,23 +32,64 @@ public class TransactionService {
 
     public Transaction addTransaction(Transaction transaction) {
         try {
+            if(CheckSufficientFunds(transaction)){
+
+                Transaction newtransaction = new Transaction(
+                    transaction.getSenderIban(),
+                    transaction.getRecieverIban(),
+                    transaction.getAmount(),
+                    "regular transaction"
+                    );
+                    return transactionRepository.save(newtransaction);
+                }
+                else {
+                    throw new ServiceException("Insufficient funds");
+                }
+        } catch (Exception ex) {
+            throw new ServiceException("Failed to add account", ex);
+        }
+    }
+    public Transaction addTransactionDeposit(Transaction transaction) {
+        try {
+            if(CheckSufficientFunds(transaction)){
             Transaction newtransaction = new Transaction(
                     transaction.getSenderIban(),
                     transaction.getRecieverIban(),
                     transaction.getAmount(),
-                    transaction.getDescription()
+                    "deposit transaction"
+            );
+            return transactionRepository.save(newtransaction);
+        }
+        else {
+            throw new ServiceException("Insufficient funds");
+        }
+        } catch (Exception ex) {
+            throw new ServiceException("Failed to add account", ex);
+        }
+    }
+    public Transaction addTransactionWithdraw(Transaction transaction) {
+        try {
+            Transaction newtransaction = new Transaction(
+                    transaction.getSenderIban(),
+                    transaction.getRecieverIban(),
+                    transaction.getAmount(),
+                    "withdraw transaction"
             );
             return transactionRepository.save(newtransaction);
         } catch (Exception ex) {
             throw new ServiceException("Failed to add account", ex);
         }
     }
-
-    public Transaction Deposit(Transaction transaction) {
-        return transactionRepository.save(transaction);
-    }
-
-    public Transaction Withdraw(Transaction transaction) {
-        return transactionRepository.save(transaction);
+    public boolean CheckSufficientFunds(Transaction transaction) {
+        //throw new ServiceException(accountService.getAccountByIban(transaction.getSenderIban()).getBalance()+"");
+        try {
+            if (transaction.getAmount() > accountService.getAccountByIban(transaction.getSenderIban()).getBalance()) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception ex) {
+            throw new ServiceException("Failed to add account", ex);
+        }
     }
 }
