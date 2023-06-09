186bb7a12fe54819e491d49188db1041338e32a1
OhannesH <90188159+OhannesH@users.noreply.github.com>
2023-06-10 01:25:38 +0200

    added commented out lines to check user token

--- src/main/java/Group6/BankingApp/Controllers/TransactionController.java
+++ src/main/java/Group6/BankingApp/Controllers/TransactionController.java
@@ -3,6 +3,10 @@ package Group6.BankingApp.Controllers;
 import Group6.BankingApp.Models.Transaction;
 import Group6.BankingApp.Services.AccountService;
 import Group6.BankingApp.Services.TransactionService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/transactions", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TransactionController {
-
+    // private static final String SECRET_KEY = "fDKiV3Rq7t";
     private  final TransactionService transactionService;
 
     public TransactionController(TransactionService transactionService) {
@@ -25,6 +29,7 @@ public class TransactionController {
 
     @GetMapping
     public ResponseEntity<List<Transaction>> getAllTransactions(
+            @RequestHeader("Authorization") String token,
             @RequestParam(defaultValue = "NL67INGB1234567890") String iban,
             @RequestParam(defaultValue = "0") Integer skip,
             @RequestParam(defaultValue = "40") Integer limit,
@@ -33,6 +38,20 @@ public class TransactionController {
             @RequestParam(defaultValue = "") String pin
     ) {
         try {
+            // Remove "Bearer " prefix from the token string
+            // String jwtToken = token.replace("Bearer ", "");
+
+            // // // Validate and parse the JWT token
+            // Claims claims = Jwts.parserBuilder()
+            //     .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+            //     .build()
+            //     .parseClaimsJws(jwtToken)
+            //     .getBody();
+
+            // // // Extract user information from the claims
+            // String userId = claims.getSubject();
+            // // Perform additional authentication and authorization checks if needed
+
             if (dateTo.isEmpty()) {
                 LocalDate oneYearAgo = LocalDate.now().minusYears(1);
                 dateFrom = oneYearAgo.toString();
@@ -79,7 +98,7 @@ public class TransactionController {
     }
 
     @PostMapping
-    public ResponseEntity addTransaction(@RequestBody Transaction transaction, @RequestParam(defaultValue = "0000") String pin) {
+    public ResponseEntity addTransaction(@RequestBody Transaction transaction, @RequestParam(defaultValue = "0000") String pin, @RequestHeader("Authorization") String token) {
         try{
             Transaction newTransaction=transactionService.addTransaction(transaction, pin);
             return ResponseEntity.status(201).body(newTransaction);
@@ -88,7 +107,7 @@ public class TransactionController {
         }
     }
     @PostMapping(value = "/deposit")
-    public ResponseEntity Deposit(@RequestBody Transaction transaction) {
+    public ResponseEntity Deposit(@RequestBody Transaction transaction, @RequestHeader("Authorization") String token) {
         try{
             Transaction newTransaction=transactionService.addTransactionDeposit(transaction);
             return ResponseEntity.status(201).body(newTransaction);
@@ -96,8 +115,9 @@ public class TransactionController {
             return ResponseEntity.internalServerError().body(e.getCause().getMessage());
         }
     }
+    
     @PostMapping(value = "/withdraw")
-    public ResponseEntity Withdraw(@RequestBody Transaction transaction, @RequestParam(defaultValue = "0000") String pin) {
+    public ResponseEntity Withdraw(@RequestBody Transaction transaction, @RequestParam(defaultValue = "0000") String pin, @RequestHeader("Authorization") String token) {
         try{
             Transaction newTransaction=transactionService.addTransactionWithdraw(transaction, pin);
             return ResponseEntity.status(201).body(newTransaction);
